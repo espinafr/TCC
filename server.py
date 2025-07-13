9958cdef572dcc923dbf934d095743df7de05e5d
espinafr <theo.ruela@gmail.com>
2025-07-13 11:46:52 -0300

    Removi coisas desnecessárias

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, flash, session
+from flask import Flask, render_template, redirect, url_for, flash, session
 from flask_wtf import CSRFProtect
 from functools import wraps
 from dotenv import load_dotenv
