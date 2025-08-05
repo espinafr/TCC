8137f9a14d177c5003dfef984e83304fad8ed1e5
théo <139395776+espinafr@users.noreply.github.com>
2025-08-05 09:22:11 -0300

    Tirei o limite da senha

--- app/database.py
+++ app/database.py
@@ -21,7 +21,7 @@ class User(Base):
     id = Column(Integer, primary_key=True, index=True)
     username = Column(String(32), unique=True, nullable=False)
     email = Column(String(100), unique=True, nullable=False)
-    password = Column(String(255), nullable=False)
+    password = Column(Text, nullable=False)
     power = Column(SmallInteger, default=0)
     creation_date = Column(DateTime, default=datetime.now)
     active = Column(Boolean, default=False)
